cd41f0feff719767c11c7ee52f8a1f024dfd7fdd
Adam Troberg <adam.troberg@elev.ga.ntig.se>
2023-01-26 18:20:43 +0100

    test

--- timer.js
+++ timer.js
@@ -1,18 +1,20 @@
-
 const timerButton = document.getElementById("timerButton");
 
 const timeRemaining = document.getElementById("timeRemaining")
 
 const startTimer = () => {
-    const timer = 1 * 10 * 1000; // 30 minutes in milliseconds
+    // Timer är 30 min och 60 sek
+    const timer = 30 * 60 * 1000; 
     const endTime = Date.now() + timer;
-    timerButton.innerHTML = ""; // remove the original button
+    timerButton.innerHTML = ""; 
     let minutes;
     let seconds;
   
     const intervalId = setInterval(() => {
       const timeLeft = endTime - Date.now();
-      if (timeLeft < 0) {
+      if (timeLeft < 0) 
+      {
+        alert("Tiden är slut! Ta ut auberginen ur ugnen!")
         timeRemaining.classList.add("hidden");
         clearInterval(intervalId);
         timerButton.innerHTML = `<button onclick="startTimer()">Klicka här för att starta timer för ugnen</button>`;
